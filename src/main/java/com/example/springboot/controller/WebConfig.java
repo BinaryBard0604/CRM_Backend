e939496191e6e84303b6b386999e0c3243b0fdfa
BinaryBard06004 <reonelronaldo@gmail.com>
2025-02-11 04:16:30 -0600

    [fix]: fix all

--- src/main/java/com/example/springboot/controller/WebConfig.java
+++ src/main/java/com/example/springboot/controller/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("https://www.timexpress.ae") // Allow this origin
+                .allowedOrigins("https://3.29.237.128") // Allow this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
                 .allowCredentials(true) // Allow credentials
                 .allowedHeaders("*"); // Allow all headers
